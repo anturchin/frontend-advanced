ba8f9ddb7759875a1604a3a0505f7425ab1845c6
ann <tur4intwo@gmail.com>
2023-12-06 20:03:25 +0300

    feat: add style themes and hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Suspense, useContext, useState } from 'react';
 import { AboutAsync } from './pages/aboutPage/About.async';
 import { MainAsync } from './pages/mainPage/Main.async';
-import './index.scss';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+import './styles/index.scss';
+
 
 const App = () => {
+
+	
+	const {theme, toggleTheme} = useTheme();
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>theme change</button> <br />
 			<Link to={'/'}>home</Link>
 			<Link to={'/about'}>about</Link>
 			<Suspense fallback={<div>loading...</div>}>
