a3f591e2271feeff29827ab366ffe035134af02e
anturchin <tur4intwo@gmail.com>
2023-12-07 15:00:37 +0300

    feat: add helpres function (classNames)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AboutAsync } from './pages/aboutPage/About.async';
 import { MainAsync } from './pages/mainPage/Main.async';
 import { Theme, ThemeContext } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 import './styles/index.scss';
 
 
@@ -12,9 +13,10 @@ const App = () => {
 
 	
 	const {theme, toggleTheme} = useTheme();
+	const bool = true;
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<button onClick={toggleTheme}>theme change</button> <br />
 			<Link to={'/'}>home</Link>
 			<Link to={'/about'}>about</Link>
